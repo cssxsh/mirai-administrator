c0f49cf3701abd3e9e4d8069ce42923f6dae1c8f
cssxsh <cssxsh@gmail.com>
2022-02-09 16:54:23 +0800

    fix: TimerService start

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/MiraiAdministrator.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/MiraiAdministrator.kt
@@ -16,6 +16,8 @@ import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.utils.*
 import xyz.cssxsh.mirai.plugin.command.*
 import xyz.cssxsh.mirai.spi.*
+import java.util.*
+import kotlin.collections.*
 import kotlin.coroutines.*
 
 /**
@@ -174,89 +176,107 @@ public object MiraiAdministrator : SimpleListenerHost() {
 
     // region Timer
 
+    private val cache: Map<TimerService<*, *>, MutableSet<ContactOrBot>> =
+        WeakHashMap<TimerService<*, *>, MutableSet<ContactOrBot>>().withDefault { HashSet() }
+
     /**
      * 启动一个群定时服务
      */
-    private fun GroupTimerService<*>.start(target: Group): Job = launch(SupervisorJob()) {
-        while (isActive && target.isActive) {
-            // 延时到 [moment]
-            delay(wait(end = moment(target) ?: break))
-            if (target.isActive.not() || target.botPermission < MemberPermission.ADMINISTRATOR) break
+    private fun GroupTimerService<*>.start(target: Group) {
+        val records = cache.getValue(this)
+        if (records.add(target).not()) return
+        launch(target.coroutineContext) {
+            while (isActive) {
+                // 延时到 [moment]
+                delay(wait(end = moment(target) ?: break))
+                if (target.isActive.not() || target.botPermission < MemberPermission.ADMINISTRATOR) break
 
-            when (this@start) {
-                is GroupAllowTimer -> launch(SupervisorJob()) {
-                    for ((id, permit) in run(target)) {
+                when (this@start) {
+                    is GroupAllowTimer -> launch(SupervisorJob()) {
+                        for ((id, permit) in run(target)) {
+                            try {
+                                if (permit) {
+                                    AbstractPermitteeId.AnyMember(target.id).permit(permissionId = id)
+                                } else {
+                                    AbstractPermitteeId.AnyMember(target.id).cancel(permissionId = id, false)
+                                }
+                            } catch (cause: Throwable) {
+                                logger.error({ "$id set failure with $id" }, cause)
+                            }
+                        }
+                    }
+                    is GroupCurfewTimer -> launch(SupervisorJob()) {
                         try {
-                            if (permit) {
-                                AbstractPermitteeId.AnyMember(target.id).permit(permissionId = id)
-                            } else {
-                                AbstractPermitteeId.AnyMember(target.id).cancel(permissionId = id, false)
+                            val mute = run(target)
+                            if (target.settings.isMuteAll != mute) {
+                                target.settings.isMuteAll = mute
                             }
                         } catch (cause: Throwable) {
-                            logger.error({ "$id set failure with $id" }, cause)
+                            logger.error({ "${target.render()} mute set failure with $id" }, cause)
                         }
                     }
-                }
-                is GroupCurfewTimer -> launch(SupervisorJob()) {
-                    try {
-                        target.settings.isMuteAll = run(target)
-                    } catch (cause: Throwable) {
-                        logger.error({ "${target.render()} mute set failure with $id" }, cause)
-                    }
-                }
-                is MemberCleaner -> launch(SupervisorJob()) {
-                    for ((member, reason) in run(target)) {
-                        try {
-                            member.kick(message = reason)
-                        } catch (cause: Throwable) {
-                            logger.error({ "${member.render()} clean failure with $id" }, cause)
+                    is MemberCleaner -> launch(SupervisorJob()) {
+                        for ((member, reason) in run(target)) {
+                            try {
+                                member.kick(message = reason)
+                            } catch (cause: Throwable) {
+                                logger.error({ "${member.render()} clean failure with $id" }, cause)
+                            }
                         }
                     }
-                }
-                is MemberNickCensor -> launch(SupervisorJob()) {
-                    val censor = run(target)
-                    for (member in target.members) {
-                        try {
-                            member.nameCard = censor(member) ?: continue
-                        } catch (cause: Throwable) {
-                            logger.error({ "${target.render()} nick set failure with $id" }, cause)
+                    is MemberNickCensor -> launch(SupervisorJob()) {
+                        val censor = run(target)
+                        for (member in target.members) {
+                            try {
+                                member.nameCard = censor(member) ?: continue
+                            } catch (cause: Throwable) {
+                                logger.error({ "${target.render()} nick set failure with $id" }, cause)
+                            }
                         }
                     }
-                }
-                is MemberTitleCensor -> launch(SupervisorJob()) {
-                    val censor = run(target)
-                    for (member in target.members) {
-                        try {
-                            member.specialTitle = censor(member) ?: continue
-                        } catch (cause: Throwable) {
-                            logger.error({ "${target.render()} title set failure with $id" }, cause)
+                    is MemberTitleCensor -> launch(SupervisorJob()) {
+                        val censor = run(target)
+                        for (member in target.members) {
+                            try {
+                                member.specialTitle = censor(member) ?: continue
+                            } catch (cause: Throwable) {
+                                logger.error({ "${target.render()} title set failure with $id" }, cause)
+                            }
                         }
                     }
-                }
-            }.invokeOnCompletion { cause ->
-                if (cause != null) {
-                    logger.error({ "${target.render()} timer run failure with $id" }, cause)
-                } else {
-                    logger.info { "${target.render()} timer run success with $id" }
+                }.invokeOnCompletion { cause ->
+                    if (cause != null) {
+                        logger.error({ "${target.render()} timer run failure with $id" }, cause)
+                    } else {
+                        logger.info { "${target.render()} timer run success with $id" }
+                    }
                 }
             }
+        }.invokeOnCompletion {
+            records.remove(target)
         }
     }
 
     /**
      * 启动一个定时消息服务
      */
-    private fun BotTimingMessage.start(from: Bot): Job = launch(SupervisorJob()) {
-        while (isActive && from.isActive) {
-            delay(wait(end = moment(from) ?: break))
+    private fun BotTimingMessage.start(from: Bot) {
+        val records = cache.getValue(this)
+        if (records.add(from).not()) return
+        launch(from.coroutineContext) {
+            while (isActive) {
+                delay(wait(end = moment(from) ?: break))
 
-            run(contact = from).onCompletion { cause ->
-                if (cause != null) {
-                    logger.error({ "${from.render()} timer run failure with $id" }, cause)
+                run(contact = from).onCompletion { cause ->
+                    if (cause != null) {
+                        logger.error({ "${from.render()} timer run failure with $id" }, cause)
+                    }
+                }.collect { receipt ->
+                    logger.info { "${from.render()} timer ${receipt.target.render()} success with $id" }
                 }
-            }.collect { receipt ->
-                logger.info { "${from.render()} timer ${receipt.target.render()} success with $id" }
             }
+        }.invokeOnCompletion {
+            records.remove(from)
         }
     }
 
