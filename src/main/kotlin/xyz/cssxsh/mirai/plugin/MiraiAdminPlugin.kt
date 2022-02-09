512d1682e7724d49a566c4d592469c342bd54a6e
cssxsh <cssxsh@gmail.com>
2022-02-09 18:22:57 +0800

    build: 1.0.3

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/MiraiAdminPlugin.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/MiraiAdminPlugin.kt
@@ -13,7 +13,7 @@ public object MiraiAdminPlugin : KotlinPlugin(
     JvmPluginDescription(
         id = "xyz.cssxsh.mirai.plugin.mirai-administrator",
         name = "mirai-administrator",
-        version = "1.0.2",
+        version = "1.0.3",
     ) {
         author("cssxsh")
     }
