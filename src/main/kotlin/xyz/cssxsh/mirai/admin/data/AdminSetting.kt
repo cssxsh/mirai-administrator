2ded8b9b859a25ed6bab22740fabc22e67bcce9b
cssxsh <cssxsh@gmail.com>
2023-02-20 13:11:39 +0800

    fix: censor dict load

--- src/main/kotlin/xyz/cssxsh/mirai/admin/data/AdminSetting.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/admin/data/AdminSetting.kt
@@ -15,12 +15,12 @@ internal object AdminSetting : ReadOnlyPluginConfig("AdminSetting"), MiraiConten
 
     internal const val OWNER_DEFAULT = 12345L
 
-    private val list: MutableMap<String, List<String>> = HashMap()
+    private val dict: MutableMap<String, List<String>> = HashMap()
 
     private val cache: MutableMap<String, Regex> = WeakHashMap()
 
     override val censorRegex: Sequence<Regex> = sequence {
-        for ((_, patterns) in list) {
+        for ((_, patterns) in dict) {
             for (pattern in patterns) {
                 if (pattern.isEmpty()) continue
                 val regex = try {
@@ -45,7 +45,7 @@ internal object AdminSetting : ReadOnlyPluginConfig("AdminSetting"), MiraiConten
             plugin.logger.info("读取审核库 ${path.fileName}")
 
             try {
-                list[path.name] = path.readLines()
+                dict[path.name] = path.readLines()
             } catch (cause: IOException) {
                 plugin.logger.warning("读取失败 $path", cause)
             }
@@ -66,24 +66,25 @@ internal object AdminSetting : ReadOnlyPluginConfig("AdminSetting"), MiraiConten
                     val path = event.context() as? Path ?: continue
                     if (!path.toString().endsWith(".txt")) continue
 
-                    when (event.kind()) {
+                    when (val kind = event.kind()) {
                         StandardWatchEventKinds.ENTRY_CREATE,
                         StandardWatchEventKinds.ENTRY_MODIFY -> {
                             val file = folder.resolve(path)
                             if (!file.exists()) {
-                                plugin.logger.info("移除审核库 $path , ${event.kind().name()}")
-                                list.remove(file.name)
+                                plugin.logger.info("移除审核库 $path , ${kind.name()}")
+                                dict.remove(file.name)
                                 continue
                             }
-                            plugin.logger.info("更新审核库 $path , ${event.kind().name()}")
+                            plugin.logger.info("更新审核库 $path , ${kind.name()}")
                             try {
-                                list[file.name] = file.readLines()
+                                dict[file.name] = file.readLines()
                             } catch (cause: IOException) {
-                                plugin.logger.warning("更新审核库 $file", cause)
+                                plugin.logger.warning("更新审核库 $file 失败 , ${kind.name()}", cause)
                             }
                         }
-                        StandardWatchEventKinds.ENTRY_CREATE -> {
-                            list.remove(path.name)
+                        StandardWatchEventKinds.ENTRY_DELETE -> {
+                            plugin.logger.info("移除审核库 $path , ${kind.name()}")
+                            dict.remove(path.name)
                         }
                         else -> Unit
                     }
