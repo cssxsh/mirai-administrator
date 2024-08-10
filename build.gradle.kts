48d94cc2f9080178de13b9d48e7c598cf12fb5b8
cssxsh <cssxsh@gmail.com>
2024-08-10 17:49:40 +0800

    update: dependencies, close #32

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.8.10"
-    kotlin("plugin.serialization") version "1.8.10"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.serialization") version "1.8.22"
 
-    id("net.mamoe.mirai-console") version "2.15.0"
-    id("me.him188.maven-central-publish") version "1.0.0-dev-3"
+    id("net.mamoe.mirai-console") version "2.16.0"
+    id("me.him188.maven-central-publish") version "1.0.0"
 }
 
 group = "xyz.cssxsh.mirai"
@@ -27,13 +27,14 @@ repositories {
 
 dependencies {
     api("com.cronutils:cron-utils:9.2.1")
-    api("jakarta.mail:jakarta.mail-api:2.1.2")
-    api("org.apache.poi:poi:5.2.3")
-    implementation("org.eclipse.angus:angus-mail:2.0.2")
-    implementation("org.apache.poi:poi-ooxml:5.2.3")
+    api("jakarta.mail:jakarta.mail-api:2.1.3")
+    // api("org.apache.poi:poi:5.3.0") fix https://github.com/cssxsh/mirai-administrator/issues/32
+    implementation("org.apache.poi:poi:5.3.0")
+    implementation("org.eclipse.angus:angus-mail:2.0.3")
+    implementation("org.apache.poi:poi-ooxml:5.3.0")
     testImplementation(kotlin("test"))
     //
-    implementation(platform("net.mamoe:mirai-bom:2.15.0"))
+    implementation(platform("net.mamoe:mirai-bom:2.16.0"))
     compileOnly("net.mamoe:mirai-core")
     compileOnly("net.mamoe:mirai-core-utils")
     compileOnly("net.mamoe:mirai-console-compiler-common")
@@ -41,11 +42,11 @@ dependencies {
     testImplementation("net.mamoe:mirai-logging-slf4j")
     testImplementation("net.mamoe:mirai-core-utils")
     //
-    implementation(platform("io.ktor:ktor-bom:2.2.4"))
+    implementation(platform("io.ktor:ktor-bom:2.3.10"))
     implementation("io.ktor:ktor-client-okhttp")
     implementation("io.ktor:ktor-client-encoding")
     //
-    implementation(platform("org.slf4j:slf4j-parent:2.0.7"))
+    implementation(platform("org.slf4j:slf4j-parent:2.0.15"))
     testImplementation("org.slf4j:slf4j-simple")
 }
 
